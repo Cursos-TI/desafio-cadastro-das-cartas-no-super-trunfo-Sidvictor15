87957839e59ce03a4f6c9a57d8e14d81592358c0
Sidvictor15 <sidneyvictor52@gmail.com>
2025-02-21 19:08:17 +0000

    projeto.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,35 +4,25 @@
 // Tema 1 - Cadastro das Cartas
 
 int main() {
-    char Estado;
-    char Codigo[];
-    char Cidade[];
-    int População;
-    float Área;
-    float PIB;
-    int Pontos;
+         
+    char Estado1 = 'A';
+    char Codigo1[20] = "A01";
+    char Cidade1[20] = "Bahia";
+    int Populacao1 = 12325000;
+    float Area1 = 567.295;
+    float PIB1 = 114.4;
+    int pontosturisticos1 = 48;
 
-    printf("Digite o Estado: \n");
-    scanf("c", &Estado);
+    printf("Estado: %c\n", Estado1);
+    printf("Codigo: %s\n", Codigo1);
+    printf("Cidade: %s\n", Cidade1);
+    printf("População: %d mil habitantes\n", Populacao1);
+    printf("Área: %.2f km²\n", Area1);
+    printf("PIB: %1f bilhôes de reais\n", PIB1);
+    printf("Pontos Turísticos: %d\n", pontosturisticos1);
 
-    printf("Digite o codigo: \n");
-    scanf("s", &Codigo);
 
-    printf("Digite o nome da cidade: \n");
-    scanf("s", &Cidade);
-
-    printf("Digite a populacao: \n");
-    scanf("%d", &População);
-
-    printf("Digite a area: \n");
-    scanf("f", &Área);
-
-    printf("Digite o PIB: \n");
-    scanf("%e", &PIB);
-
-    printf("Digite o numero de Pontos Turísticos: \n");
-    scanf("%d", &Pontos);
-
+    
 
     return 0;
 }
